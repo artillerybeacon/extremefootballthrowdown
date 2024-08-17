bdf181ca31d44fd40b0467de6fa4ac899374323b
ATP-Kraken <34113921+ATP-Kraken@users.noreply.github.com>
2024-08-16 22:50:21 -0400

    Update cl_deathnotice.lua due to GMOD changes

--- gamemodes/fretta13/gamemode/cl_deathnotice.lua
+++ gamemodes/fretta13/gamemode/cl_deathnotice.lua
@@ -1,5 +1,6 @@
 /*
 	Start of the death message stuff.
+	2024: Compatibility with base gamemode changes
 */
 
 include( 'vgui/vgui_gamenotice.lua' )
@@ -9,7 +10,7 @@ local function CreateDeathNotify()
 	local x, y = ScrW(), ScrH()
 
 	g_DeathNotify = vgui.Create( "DNotify" )
-	
+
 	g_DeathNotify:SetPos( 0, 25 )
 	g_DeathNotify:SetSize( x - ( 25 ), y )
 	g_DeathNotify:SetAlignment( 9 )
@@ -29,10 +30,10 @@ local function RecvPlayerKilledByPlayer( length )
 
 	if ( !IsValid( attacker ) ) then return end
 	if ( !IsValid( victim ) ) then return end
-	
-	GAMEMODE:AddDeathNotice( victim, inflictor, attacker )	
+
+	GAMEMODE:AddDeathNotice( attacker:Name(), attacker:Team(), inflictor, victim:Name(), victim:Team() )
 end
-	
+
 net.Receive( "PlayerKilledByPlayer", RecvPlayerKilledByPlayer )
 
 
@@ -42,10 +43,10 @@ local function RecvPlayerKilledSelf( length )
 
 	if ( !IsValid( victim ) ) then return end
 
-	GAMEMODE:AddPlayerAction( victim, GAMEMODE.SuicideString )
+	GAMEMODE:AddPlayerAction( victim:Name(), GAMEMODE.SuicideString )
 
 end
-	
+
 net.Receive( "PlayerKilledSelf", RecvPlayerKilledSelf )
 
 
@@ -53,28 +54,29 @@ local function RecvPlayerKilled( length )
 
 	local victim 	= net.ReadEntity()
 	local inflictor	= net.ReadString()
-	local attacker 	= "#" .. net.ReadString()
+	local attacker 	= net.ReadString()
 
+	print("RecvPlayerKilled")
 	if ( !IsValid( victim ) ) then return end
-			
-	GAMEMODE:AddDeathNotice( victim, inflictor, attacker )
+
+	GAMEMODE:AddDeathNotice( "#" .. attacker, -1, inflictor, victim:Name(), victim:Team() )
 
 end
-	
+
 net.Receive( "PlayerKilled", RecvPlayerKilled )
 
 local function RecvPlayerKilledNPC( length )
 
-	local victim 	= "#" .. net.ReadString()
+	local victim 	= net.ReadString()
 	local inflictor	= net.ReadString()
 	local attacker 	= net.ReadEntity()
 
 	if ( !IsValid( attacker ) ) then return end
-			
-	GAMEMODE:AddDeathNotice( victim, inflictor, attacker )
+
+	GAMEMODE:AddDeathNotice( attacker, attacker:Team(), inflictor, "#" .. victim, 0 )
 
 end
-	
+
 net.Receive( "PlayerKilledNPC", RecvPlayerKilledNPC )
 
 
@@ -83,8 +85,8 @@ local function RecvNPCKilledNPC( length )
 	local victim 	= "#" .. net.ReadString()
 	local inflictor	= net.ReadString()
 	local attacker 	= "#" .. net.ReadString()
-		
-	GAMEMODE:AddDeathNotice( victim, inflictor, attacker )
+
+	GAMEMODE:AddDeathNotice( attacker, -1, inflictor, victim, -1 )
 
 end
 
@@ -92,25 +94,32 @@ net.Receive( "NPCKilledNPC", RecvNPCKilledNPC )
 
 
 /*---------------------------------------------------------
-   Name: gamemode:AddDeathNotice( Victim, Weapon, Attacker )
+   Name: gamemode:AddDeathNotice( Attacker, team1, Inflictor, Victim, team2, flags )
    Desc: Adds an death notice entry
 ---------------------------------------------------------*/
-function GM:AddDeathNotice( victim, inflictor, attacker )
+function GM:AddDeathNotice( attacker, team1, inflictor, victim, team2 )
 
 	if ( !IsValid( g_DeathNotify ) ) then return end
-
-	local pnl = vgui.Create( "GameNotice", g_DeathNotify )
 	
-	pnl:AddText( attacker )
-	pnl:AddIcon( inflictor )
-	pnl:AddText( victim )
-	
-	g_DeathNotify:AddItem( pnl )
+	if inflictor and inflictor != "suicide" then
 
+		local pnl = vgui.Create( "GameNotice", g_DeathNotify )
+
+		pnl:AddText( attacker or "", GAMEMODE:GetTeamNumColor(attackerTeam) )
+		pnl:AddIcon( inflictor )
+		pnl:AddText( victim or "", GAMEMODE:GetTeamNumColor(victimTeam) )
+
+		g_DeathNotify:AddItem( pnl )
+
+	else
+		--We need to handle suicides within this due to basegame deprecation
+		
+		GAMEMODE:AddTeamPlayerAction( victim, GAMEMODE.SuicideString, team2 )
+	end
 end
 
 function GM:AddPlayerAction( ... )
-	
+
 	if ( !IsValid( g_DeathNotify ) ) then return end
 
 	local pnl = vgui.Create( "GameNotice", g_DeathNotify )
@@ -118,10 +127,26 @@ function GM:AddPlayerAction( ... )
 	for k, v in ipairs({...}) do
 		pnl:AddText( v )
 	end
-	
+
 	// The rest of the arguments should be re-thought.
 	// Just create the notify and add them instead of trying to fit everything into this function!???
-	
+
 	g_DeathNotify:AddItem( pnl )
-	
+
+end
+
+function GM:AddTeamPlayerAction( subject, action, teamnum )
+
+	if ( !IsValid( g_DeathNotify ) ) then return end
+
+	local pnl = vgui.Create( "GameNotice", g_DeathNotify )
+
+	pnl:AddText( subject, GAMEMODE:GetTeamNumColor(teamnum) )
+	pnl:AddText( action )
+
+	// The rest of the arguments should be re-thought.
+	// Just create the notify and add them instead of trying to fit everything into this function!???
+
+	g_DeathNotify:AddItem( pnl )
+
 end
